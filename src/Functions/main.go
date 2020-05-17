a8e8f9ecfb06d9e29f2b09ef9e80554f6a6a70b6
vidhlakh <vidhlakh@gmail.com>
2020-05-17 16:24:47 -0700

    Update Function PArameter passing and Challenge

--- src/Functions/main.go
+++ src/Functions/main.go
@@ -11,8 +11,33 @@ func main(){
 	fmt.Println("c=",c)
 	div,rem := divmod(a,b)
 	fmt.Println("Result=",div,"Reminder=",rem)
-	//Using DoubleValue Function
+	//Using DoubleValue Function . Go pass Slice, Map by reference . 
+	// Without return the value , values are changed
 	values:=[]int {1,2,3,4}
 	doubleValue(values,2)
-	fmt.Println(values)
+	fmt.Println("Value Changed as it is passed by Reference",values)
+	//Integer pass by value 
+	val:=20
+	double(val)
+	fmt.Println("Value not changes as it is passed by Value",val)
+	doublePtr(&val)
+	fmt.Println("Value passing the pointer ",val)
+
+	//Return value and error 
+	sq , err := sqroot(-2)
+	if err==nil{
+		fmt.Println("Sqroot value",sq)
+	}else{
+		fmt.Println("error:",err)
+	}
+	//Using Defer
+	worker()
+
+	//Challenge 
+	contentHeader,error:= contentType("https://golang.org")
+	if error==nil{
+		fmt.Println("Contt",contentHeader)
+	}else{
+		fmt.Println("ErRorr: ",error)
+	}
 }
