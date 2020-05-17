c1db63e8427c1e40d16e0ce452253ef240f423c3
vidhlakh <vidhlakh@gmail.com>
2020-05-17 09:34:34 -0700

    Update Even ended

--- EvenOdd Challenge/EvenEndedOddEnded.go
+++ EvenOdd Challenge/EvenEndedOddEnded.go
@@ -16,4 +16,14 @@ multi:=`This is
 Multiline
 String`
 fmt.Println(multi)
+//Even ended number 1001
+number:= 1002
+numString:=fmt.Sprintf("%d",number)
+fmt.Printf("%s type of (%T)\n",numString,numString) 
+if numString[0]==numString[len(numString)-1]{
+	fmt.Println("Even ended")
+}else{
+	fmt.Println("Odd Ended")
+}
+ 
 }
