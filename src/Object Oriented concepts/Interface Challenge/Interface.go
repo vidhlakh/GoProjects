5df10240af13b7e56ef717fc666a11db1d98c25f
vidhlakh <vidhlakh@gmail.com>
2020-05-19 07:54:35 -0700

    Implemment io.Writer Interface Challenge

--- src/Object Oriented concepts/Interface Challenge/Interface.go
+++ src/Object Oriented concepts/Interface Challenge/Interface.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+type Capper struct {
+	wtr io.Writer
+}
+
+func (c *Capper) Write(p []byte) (n int, err error) {
+	diff := byte('a' - 'A')
+	out := make([]byte, len(p))
+	for i, c := range p {
+		if c >= 'a' && c <= 'z' {
+			c -= diff
+		}
+		out[i] = c
+	}
+	return c.wtr.Write(out)
+
+}
+func main() {
+	c := &Capper{os.Stdout}
+	fmt.Fprintln(c, "hello")
+}
