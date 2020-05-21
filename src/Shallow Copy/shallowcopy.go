21ed4026a9f2d5482f96c274a8f07ff84a8861e6
vidhlakh <vidhlakh@gmail.com>
2020-05-21 14:55:34 -0400

    update array

--- src/Shallow Copy/shallowcopy.go
+++ src/Shallow Copy/shallowcopy.go
@@ -17,13 +17,19 @@ func main() {
 	fmt.Printf("%+v\n", p2) // {name:Alena}
 
 	// change the copied name
-	p2.name = "Not Alena"
+	p2.name = "Not Alena changed in 2"
 
-	fmt.Printf("%+v\n", p)  // {name:Alena}
-	fmt.Printf("%+v\n", p2) // {name: Not Alena}
+	fmt.Printf("p %+v\n", p)   // {name:Alena}
+	fmt.Printf("p2 %+v\n", p2) // {name: Not Alena}
 	// change the p name. Changes will be affected on both
 
-	p.name = "Not Alena"
-	fmt.Printf("%+v\n", p)  // {name:A Not lena}
-	fmt.Printf("%+v\n", p2) // {name: Not Alena}
+	p.name = "Not Alena changed in p"
+	fmt.Printf("p %+v\n", p)   // {name:A Not lena}
+	fmt.Printf("p2 %+v\n", p2) // {name: Not Alena}
+
+	//Array in assignment also memory address is different as arrays are value types 
+	a := [...]int{1, 2, 3, 4, 5}
+	b := a
+	fmt.Println("a:", a, "mem ", &a[0])
+	fmt.Println("b: ", b, "mem", &b[0])
 }
